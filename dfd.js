c0b82014d10dfafae247e073e136e0fdbbd7daef
Debargha Nandi <debarghanandi.india@gmail.com>
2025-02-21 00:08:46 +0530

    UPDATE V16

--- dfd.js
+++ dfd.js
@@ -1,61 +1,25 @@
-// Degrees of Freedom (df)
-let dfTreatment = treatCount - 1;
-let dfReplication = repCount - 1;
-let dfError = dfTreatment * dfReplication;
-
-// Mean Sum of Squares (MSS)
-let meanTreatmentSS = finalTreatmentSum / dfTreatment;
-let meanReplicationSS = finalReplicationSum / dfReplication;
-let meanErrorSS = errorSumOfSquares / dfError;
-
-// F-Calculated (Fcal)
-let fcalTreatment = meanTreatmentSS / meanErrorSS;
-let fcalReplication = meanReplicationSS / meanErrorSS;
-
-// Generate ANOVA Table
-let anovaTableHTML = `
-  <h3>ANOVA Table for Location ${i}</h3>
-  <table>
-    <tr>
-      <th>Source of Variation</th>
-      <th>Degree of Freedom (d.f)</th>
-      <th>Sum of Square (SS)</th>
-      <th>Mean Sum of Square (MSS)</th>
-      <th>F<sub>cal</sub></th>
-      <th>F<sub>MSS</sub></th>
-    </tr>
-    <tr>
-      <td><b>Treatment (T)</b></td>
-      <td>${dfTreatment}</td>
-      <td>${finalTreatmentSum.toFixed(2)}</td>
-      <td>${meanTreatmentSS.toFixed(2)}</td>
-      <td>${fcalTreatment.toFixed(2)}</td>
-      <td>${meanTreatmentSS.toFixed(2)}</td>
-    </tr>
-    <tr>
-      <td><b>Replication (R)</b></td>
-      <td>${dfReplication}</td>
-      <td>${finalReplicationSum.toFixed(2)}</td>
-      <td>${meanReplicationSS.toFixed(2)}</td>
-      <td>${fcalReplication.toFixed(2)}</td>
-      <td>-</td>
-    </tr>
-    <tr>
-      <td><b>Error (E)</b></td>
-      <td>${dfError}</td>
-      <td>${errorSumOfSquares.toFixed(2)}</td>
-      <td>${meanErrorSS.toFixed(2)}</td>
-      <td>-</td>
-      <td>${meanErrorSS.toFixed(2)}</td>
-    </tr>
-  </table>
-`;
-
-// Insert the ANOVA Table in the location section
-locationSection.insertAdjacentHTML("beforeend", anovaTableHTML);
-
-
-
-
-
-let errorSumOfSquares = totalSumOfSquares - (finalTreatmentSum + finalReplicationSum);
+body {
+    font-family: Arial, sans-serif;
+    margin: 20px;
+  }
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+  .location-section {
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 5px;
+  }
+  table,
+  th,
+  td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    padding: 5px;
+    text-align: center;
+  }
+  .summary {
+    margin-top: 10px;
+  }
